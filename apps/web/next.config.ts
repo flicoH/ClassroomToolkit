d36e3780bbcf86b3e3b7852b2d548aa8f2a033cd
flicoH <1020149196@qq.com>
2026-08-23 11:21:04 +0800

    fix: courect rewrites proxy

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@
  * @Author: huangqinjia huangqinjia
  * @Date: 2026-04-21 12:03:58
  * @LastEditors: flicoH
- * @LastEditTime: 2026-08-23 10:54:21
+ * @LastEditTime: 2026-08-23 11:20:43
  * @FilePath: /ClassroomToolkit/apps/web/next.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,6 +22,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
+        credentials: "include",
         headers: [{ key: "X-Robots-Tag", value: "noindex, nofollow, noarchive" }]
       },
       {
